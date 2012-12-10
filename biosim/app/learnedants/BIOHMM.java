1aa00357a8fb5bab4c9d4bd7a30c0a798ad0028e
Brian Hrolenok <brian.hrolenok@gmail.com>
2012-12-09 23:05:14 -0500

    Debugging convergence.

--- biosim/app/learnedants/BIOHMM.java
+++ biosim/app/learnedants/BIOHMM.java
@@ -9,10 +9,10 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 
 public class BIOHMM{
-	double[][][] transitionFunction;
+	public double[][][] transitionFunction;
 	Object[][][] transitionMonitors;
-	double[] prior;
-	int[] partition;
+	public double[] prior;
+	public int[] partition;
 	String[] binarySwitchNames;
 	BTFData data;
 	int dim, numThreads = 4;
@@ -220,6 +220,18 @@ public class BIOHMM{
 		}		
 	}
 	
+	public void updatePartition(ArrayList<Integer> seq, double[][] gamma, int[] newPartition){
+		for(int t=0;t<gamma.length;t++){
+			int max_state = 0;
+			for(int i=1;i<gamma[t].length;i++){
+				if(gamma[t][i] > gamma[t][max_state]){
+					max_state = i;
+				}
+			}
+			newPartition[seq.get(t)] = max_state;
+		}
+	}
+	
 	public void updatePartition(ArrayList<Integer> seq, KernelDensityEstimator[] b, int[] newPartition){
 		double[][] delta = new double[seq.size()][prior.length];
 		int[][] psi = new int[seq.size()][prior.length];
@@ -339,7 +351,8 @@ public class BIOHMM{
 									updateTransitions(seq,newTransitionNumerator, newTransitionDenominator, xi, gamma);
 								//}
 								//synchronized(newPartition){
-									updatePartition(seq, b, newPartition);
+									//updatePartition(seq, b, newPartition);
+									updatePartition(seq, gamma, newPartition);
 								//}
 								System.out.println("Sequence "+tmpIdx+" completed");
 								loglike[tmpIdx] = calculateSeqLogLikelihood(hat_alpha,coeff_c);
@@ -359,297 +372,7 @@ public class BIOHMM{
 			}catch(InterruptedException ie){
 				throw new RuntimeException(ie);
 			}
-			/* */
-			/*
-			//for each sequence:
-			for(int s=0;s<sequences.size();s++){
-				System.out.println("\tSequence "+s);
-				ArrayList<Integer> seq = sequences.get(s);
-				SingleSequenceUpdate ssu = new SingleSequenceUpdate(	this,
-																		seq,
-																		b,
-																		newPrior,
-																		newTransitionNumerator,
-																		newTransitionDenominator,
-																		newPartition,
-																		sequences.size());
-				try{
-					Thread t = new Thread(ssu);
-					t.start();
-					t.join();
-				} catch(Exception e){
-					throw new RuntimeException(e);
-				}
-				*/
-				/*
-				//compute alpha_t(j), all t, all j
-				System.out.println("\t Computing alpha");
-				double[][] alpha = new double[seq.size()][prior.length];
-				for(int j=0;j<prior.length;j++){
-					alpha[0][j] = prior[j]*b[j].estimate(getDataAtIDX(seq.get(0)),bandwidth);
-				}
-				for(int t = 1; t<sequences.get(s).size(); t++){
-					for(int j=0;j<prior.length;j++){
-						double sum = 0.0;
-						for(int i=0;i<prior.length;i++){
-							sum += alpha[t-1][i]*transitionFunction[i][j][getSwitchAtIDX(seq.get(t-1))];
-						}
-						sum = sum * b[j].estimate(getDataAtIDX(seq.get(t)),bandwidth);
-					}
-				}
-				*/
-				/*
-				//Scaled variables, using equations taken from
-				//"An Erratum for 'A Tutorial on Hidden Markov Models and Selected Applications
-				//in Speech Recognition'"
-				System.out.println("\t Computing scaled alpha");
-				//double[] bar_alpha = new double[prior.length];
-				double[][] hat_alpha = new double[seq.size()][prior.length];
-				double[] coeff_c = new double[seq.size()];
-				calculateScaledAlpha(seq,b,hat_alpha,coeff_c);
-				*/
-				/*
-				for(int t=0;t<seq.size();t++) coeff_c[t] = 0.0;
-				for(int j=0;j<prior.length;j++){
-					bar_alpha[j] = prior[j]*b[j].estimate(getDataAtIDX(seq.get(0)),bandwidth);
-					coeff_c[0] += bar_alpha[j];
-				}
-				coeff_c[0] = 1.0/coeff_c[0];
-				for(int j=0;j<prior.length;j++){
-					hat_alpha[0][j] = coeff_c[0] * bar_alpha[j];
-				}
-				//now do DP
-				for(int t=1;t<sequences.get(s).size();t++){
-					for(int j=0;j<prior.length;j++){
-						bar_alpha[j] = 0.0;
-						for(int i=0;i<prior.length;i++){
-							double tmp = hat_alpha[t-1][i];
-							tmp = tmp*transitionFunction[i][j][getSwitchAtIDX(seq.get(t-1))];
-							tmp = tmp*b[j].estimate(getDataAtIDX(seq.get(t)),bandwidth);
-							bar_alpha[j] += tmp;
-						}
-						coeff_c[t] += bar_alpha[j];
-					}
-					coeff_c[t] = 1.0/coeff_c[t];
-					for(int j=0;j<prior.length;j++){
-						hat_alpha[t][j] = coeff_c[t] * bar_alpha[j];
-					}
-				}
-				*/
-				/*
-				//compute beta_t(i), all t, all i
-				System.out.println("\t Computing beta");
-				double[][] beta = new double[seq.size()][prior.length];
-				for(int i=0;i<prior.length;i++){
-					beta[seq.size()-1][i] = 1.0;
-				}
-				for(int t = seq.size()-2;t >= 0;t--){
-					for(int i=0;i<prior.length;i++){
-						double sum = 0.0;
-						for(int j=0;j<prior.length;j++){
-							double tmp = transitionFunction[i][j][getSwitchAtIDX(seq.get(t))];
-							tmp *= b[j].estimate(getDataAtIDX(seq.get(t+1)),bandwidth);
-							tmp *= beta[t+1][j]; 
-							sum += tmp;
-						}
-						beta[t][i] = sum;
-					}
-				}
-				*/
-				/*
-				System.out.println("\t Computing scaled beta");
-				//double[] bar_beta = new double[prior.length];
-				double[][] hat_beta = new double[seq.size()][prior.length];
-				calculateScaledBeta(seq,b,hat_beta,coeff_c);
-				*/
-				/*
-				for(int i=0;i<prior.length;i++){
-					bar_beta[i] = 1.0;
-					hat_beta[seq.size()-1][i] = coeff_c[seq.size()-1]*bar_beta[i];
-				}
-				//now do DP
-				for(int t=seq.size()-2;t>=0;t--){
-					for(int j=0;j<prior.length;j++){
-						bar_beta[j] = 0.0;
-						for(int i=0;i<prior.length;i++){
-							double tmp = transitionFunction[i][j][getSwitchAtIDX(seq.get(t))];
-							tmp = tmp * b[j].estimate(getDataAtIDX(seq.get(t+1)),bandwidth);
-							tmp = tmp * hat_beta[t+1][i];
-							bar_beta[j] += tmp;
-						}
-						hat_beta[t][j] = coeff_c[t]*bar_beta[j];
-					}
-				}
-				*/
-				/*
-				//now that we have scaled alpha and beta, we can compute
-				//xi then gamma from the scaled values
-				System.out.println("\t Computing xi");
-				double[][][] xi = new double[seq.size()][prior.length][prior.length];
-				calcXiFromScaled(seq,b,hat_alpha,hat_beta,xi);
-				*/
-				/*
-				for(int t=0;t<seq.size();t++){
-					for(int i=0;i<prior.length;i++){
-						for(int j=0;j<prior.length;j++){
-							//the beta check goes one step past the end of the
-							//sequences, so we set our probability of 
-							//transitioning from i at time T to j at time T+1
-							//equal to 1.0, just like we do for beta
-							if(t == (seq.size()-1)){
-								xi[t][i][j] = 1.0;
-							} else {
-								xi[t][i][j] = hat_alpha[t][i];
-								xi[t][i][j] *= transitionFunction[i][j][getSwitchAtIDX(seq.get(t))];
-								xi[t][i][j] *= b[j].estimate(getDataAtIDX(seq.get(t+1)),bandwidth);
-								xi[t][i][j] *= hat_beta[t+1][j];
-							}
-						}
-					}
-				}
-				*/
-				/*
-				System.out.println("\t Computing gamma from xi");
-				//compute gamma from xi
-				double[][] gamma = new double[seq.size()][prior.length];
-				calcGammaFromXi(xi,gamma);
-				*/
-				/*
-				for(int t=0;t<seq.size();t++){
-					for(int i=0;i<prior.length;i++){
-						gamma[t][i] = 0.0;
-						for(int j=0;j<prior.length;j++){
-							gamma[t][i] += xi[t][i][j];
-						}
-					}
-				}
-				*/
-				
-				/*
-				//compute gamma_t(i) for all t, for all i
-				System.out.println("\t Computing gamma");
-				double[][] gamma = new double[seq.size()][prior.length];
-				for(int t=0;t<seq.size();t++){
-					double sum = 0.0;
-					for(int j=0;j<prior.length;j++){
-						sum+= alpha[t][j]*beta[t][j];
-					}
-					for(int i=0;i<prior.length;i++){						
-						gamma[t][i] = (alpha[t][i]*beta[t][i])/(sum);
-					}
-				}
-				//compute xi_t(i,j) for all t, for all i, for all j
-				System.out.println("\t Computing xi");
-				double[][][] xi = new double[seq.size()][prior.length][prior.length];
-				//initialize to zero since we don't hit everything
-				for(int i=0;i<prior.length;i++){
-					for(int j=0;j<prior.length;j++){
-						for(int t=0;t<seq.size();t++){
-							xi[t][i][j] = 0.0;
-						}
-					}
-				}
-				for(int t=0;t<seq.size()-1;t++){
-					double sum = 0.0;
-					for(int i=0;i<prior.length;i++){
-						for(int j=0;j<prior.length;j++){
-							double tmp = alpha[t][i];
-							tmp *= transitionFunction[i][j][getSwitchAtIDX(seq.get(t))];
-							tmp *= b[j].estimate(getDataAtIDX(seq.get(t+1)),bandwidth);
-							tmp *= beta[t+1][j];
-							sum += tmp;
-						}
-					}
-					for(int i=0;i<prior.length;i++){
-						for(int j=0;j<prior.length;j++){
-							xi[t][i][j] = alpha[t][i];
-							xi[t][i][j] *= transitionFunction[i][j][getSwitchAtIDX(seq.get(t))];
-							xi[t][i][j] *= b[j].estimate(getDataAtIDX(seq.get(t+1)),bandwidth);
-							xi[t][i][j] *= beta[t+1][j];
-							xi[t][i][j] = xi[t][i][j]/sum;
-						}
-					}
-				}
-				*/
-				/*
-				//compute prior
-				System.out.println("\t Updating prior");
-				updatePrior(newPrior,gamma,sequences.size());
-				*/
-				/*
-				for(int i=0;i<prior.length;i++){
-					//remember, we're doing this over a 
-					//number of sequences
-					newPrior[i] += gamma[0][i]/sequences.size();
-				}
-				*/
-				/*
-				System.out.println("\t Updating transition table");
-				//compute transitionFunction
-				//accumulate numerator and denom seperately
-				updateTransitions(seq, newTransitionNumerator, newTransitionDenominator, xi, gamma);
-				*/
-				/*
-				for(int i=0;i<prior.length;i++){
-					for(int j=0;j<prior.length;j++){
-						for(int k=0;k<newTransition[i][j].length;k++){
-							double numSum = 0.0, denomSum = 0.0;
-							for(int t=0;t<seq.size();t++){
-								if(k== getSwitchAtIDX(seq.get(t))){
-									numSum += xi[t][i][j];
-									denomSum += gamma[t][i];
-								}
-							}
-							newTransitionNumerator[i][j][k] += numSum;
-							newTransitionDenominator[i][j][k] +=denomSum;
-						}
-					}
-				}
-				*/
-				/*
-				//compute veterbi, outputFunction
-				System.out.println("\t Computing viterbi");
-				//since we only update one patch of data per
-				//sequence, we don't have to do anything different here
-				updatePartition(seq,b,newPartition);
-				*/
-				/*
-				double[][] delta = new double[seq.size()][prior.length];
-				int[][] psi = new int[seq.size()][prior.length];
-				for(int i=0;i<prior.length;i++){
-					delta[0][i] = prior[i]*b[i].estimate(getDataAtIDX(seq.get(0)),bandwidth);
-					psi[0][i] = 0;
-				}
-				for(int t=1;t<seq.size();t++){
-					for(int j=0;j<prior.length;j++){
-						int maxState = 0;
-						double max = delta[t-1][0], tmp;
-						max *= transitionFunction[0][j][getSwitchAtIDX(seq.get(t-1))];
-						for(int i=1;i<prior.length;i++){
-							tmp = delta[t-1][i] * transitionFunction[i][j][getSwitchAtIDX(seq.get(t-1))];
-							if(tmp > max){ 
-								max = tmp;
-								maxState = i;
-							}
-						}
-						delta[t][j] = max * b[j].estimate(getDataAtIDX(seq.get(t)),bandwidth);
-						psi[t][j] = maxState;
-					}
-				}
-				int tmpMaxState = 0;
-				for(int i=0;i<prior.length;i++){
-					if(delta[seq.size()-1][i] > delta[seq.size()-1][tmpMaxState]){
-						tmpMaxState = i;
-					}
-				}
-				newPartition[seq.get(seq.size()-1)] = tmpMaxState;
-				for(int t = seq.size()-2;t >=0;t--){
-					newPartition[seq.get(t)] = psi[t+1][newPartition[seq.get(t+1)]];
-				}
-				*/
-				//System.out.println("\t Log likelihood of sequence: "+calculateSeqLogLikelihood(hat_alpha,coeff_c));
-			//}
-			//now recombine the numerator and denominator for the transition function
+			
 			for(int i=0;i<prior.length;i++){
 				for(int j=0;j<prior.length;j++){
 					for(int k=0;k<newTransition[i][j].length;k++){
@@ -685,7 +408,8 @@ public class BIOHMM{
 			System.out.println("Sum log-likelihood of all sequences: "+llsum);
 			
 			//if(priorDifference < epsilon && transitionDiff < epsilon && percentPartChanged < epsilon) converged = true;
-			if(prevLL!= null && eps.compareTo(llsum.subtract(prevLL)) > 0) converged = true;
+			if(prevLL!= null && eps.compareTo(llsum.subtract(prevLL).abs()) > 0) converged = true;
+			if(prevLL.compareTo(llsum) > 0) System.out.println("Log-likelihood DECREASING!");
 			prevLL = llsum;
 			prior = newPrior;
 			transitionFunction = newTransition;
@@ -751,6 +475,34 @@ public class BIOHMM{
 		return sequences;
 	}
 	
+	public void writeParameters(File parameterFile){
+		FileWriter outf = new FileWriter(parameterFile);
+		//write prior
+		outf.write(prior.length+"\n");
+		for(int i=0;i<prior.length;i++){
+			outf.write(prior[i]+" ");
+		}
+		outf.write("\n");
+		//write transition function
+		outf.write(transitionFunction.length+" ");
+		outf.write(transitionFunction[0].length+" ");
+		outf.write(transitionFunction[0][0].length+"\n");
+		for(int i=0;i<transitionFunction.length;i++){
+			for(int j=0;j<transitionFunction[i].length;j++){
+				for(int k=0;k<transitionFunction[i][j].length;k++){
+					outf.write(transitionFunction[i][j][k]+" ");
+				}
+			}
+		}
+		outf.write("\n");
+		//write partition
+		outf.write(partition.length+"\n");
+		for(int i=0;i<partition.length;i++){
+			outf.write(partition[i]+" ");
+		}
+		outf.write("\n");
+	}
+	
 	public static void main(String[] args){
 		if(args.length != 1){
 			System.out.println("Usage: java BIOHMM <btfDirectory>");
@@ -764,58 +516,34 @@ public class BIOHMM{
 				sequences = biohmm.getSequences(btf);
 				System.out.println("Num sequences:"+sequences.size());
 				biohmm.learn(sequences,0.01);
+				System.out.println("Prior:");
+				System.out.print("\t[ ");
+				for(int i =0;i<biohmm.prior.length;i++){
+					System.out.print(biohmm.prior[i]+" ");
+				}
+				System.out.println("]");
+				System.out.println("Transition matrix:");
+				for(int k=0;k<biohmm.transitioinFunction[0][0].length;k++){
+					System.out.println("Binary switch: "+k);
+					for(int i=0;i<biohmm.transitionFunction.length;i++){
+						System.out.print("\t [");
+						for(int j=0;j<biohmm.transitionFunction[i].length;j++){
+							System.out.print(" "+biohmm.transitionFunction[i][j][k]);
+						}
+						System.out.println(" ]");
+					}
+				}
+				System.out.println("Partition:");
+				System.out.print("[");
+				for(int i=0;i<biohmm.partition.length;i++){
+					System.out.print(" "+biohmm.partition[i]);
+				}
+				System.out.println(" ]");
 			} catch(Exception e){
 				throw new RuntimeException(e);
 			}
 		}
 	}
 	
-	public class SingleSequenceUpdate implements Runnable {
-		public BIOHMM parent;
-		public ArrayList<Integer> seq;
-		public KernelDensityEstimator[] b;
-		public double[] newPrior;
-		public double[][][] newTransitionNumerator, newTransitionDenominator;
-		public int[] newPartition;
-		public int numSequences;
-		public SingleSequenceUpdate(	BIOHMM parent, 
-										ArrayList<Integer> seq, 
-										KernelDensityEstimator[] b,
-										double[] newPrior,
-										double[][][] newTransitionNumerator,
-										double[][][] newTransitionDenominator,
-										int[] newPartition,
-										int numSequences){ 
-			this.parent = parent;
-			this.seq = seq;
-			this.b = b;
-			this.newPrior = newPrior;
-			this.newTransitionNumerator = newTransitionNumerator;
-			this.newTransitionDenominator = newTransitionDenominator;
-			this.newPartition = newPartition;
-			this.numSequences = numSequences;
-		}
-		public void run(){
-			double[][] hat_alpha = new double[seq.size()][parent.prior.length];
-			double[] coeff_c = new double[seq.size()];
-			parent.calculateScaledAlpha(seq, b, hat_alpha, coeff_c);
-			double[][] hat_beta = new double[seq.size()][prior.length];
-			parent.calculateScaledBeta(seq,b,hat_beta,coeff_c);
-			double[][][] xi = new double[seq.size()][parent.prior.length][parent.prior.length];
-			calcXiFromScaled(seq,b,hat_alpha,hat_beta,xi);
-			double[][] gamma = new double[seq.size()][parent.prior.length];
-			calcGammaFromXi(xi,gamma);
-			synchronized(newPrior){
-				parent.updatePrior(newPrior, gamma, numSequences);
-			}
-			synchronized(newTransitionNumerator){
-				parent.updateTransitions(seq,newTransitionNumerator, newTransitionDenominator, xi, gamma);
-			}
-			synchronized(newPartition){
-				parent.updatePartition(seq,b,newPartition);
-			}
-			System.out.println("Log likelihood of sequence: "+parent.calculateSeqLogLikelihood(hat_alpha,coeff_c));
-		}
-	}
 	
 }
